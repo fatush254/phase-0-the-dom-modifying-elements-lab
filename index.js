5c7b342dc33fa23de29073e523a079bc9f735068
fatush254 <fgalgalo945@gmail.com>
2024-03-31 23:19:03 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+var elementToRemove = document.getelementaryById('main');
+var parentElement = elementToRemove.parentNode;
+parentElement.removeChild(elementToRemove);
+
+var newHeader = document.createElement("h1")
+newHeader.textContent = 'This is a new header';
+newHeader.id ="victory"
+document.body.appendChild(newHeader)
+
+var newHeader = document.createElement("h1")
+newHeader.textContent = 'fatush wins';
+newHeader.id ="victory"
+document.body.appendChild(newHeader);
